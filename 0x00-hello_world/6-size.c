ef3c5f5deadb2becb928f1e95d9478aa27eb2018
Geemonic <ogechi.okorochukwu@gmail.com>
2023-06-17 04:38:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return 0 (Success)
  */
 int main(void)
-(       	
+{        
 	char a;
 	int b;
 	long int c;
@@ -17,4 +17,4 @@ printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
-)
+}
