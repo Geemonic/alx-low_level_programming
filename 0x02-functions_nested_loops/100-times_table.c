3e41cffb5d22e7d75847fd257388487f438dbeb9
Geemonic <ogechi.okorochukwu@gmail.com>
2023-06-22 11:44:06 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints_times_table - Prints the times table of the input,
+ * print_times_table - Prints the times table of the input,
  * starting with 0.
  * @n: The value of the times table to be printed.
  */
@@ -23,10 +23,12 @@ void print_times_table(int n)
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
+					_putchar(((prod / 100)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
@@ -34,8 +36,7 @@ void print_times_table(int n)
 				}
 				_putchar((prod % 10) + '0');
 			}
-			_putchar((prod % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
