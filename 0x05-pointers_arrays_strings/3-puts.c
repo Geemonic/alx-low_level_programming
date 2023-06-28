23bd598f365046cf550d7ca2bf0e1481120607b6
Geemonic <ogechi.okorochukwu@gmail.com>
2023-06-28 06:41:50 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string, followed by a new line, and stdout
  * @str: string to print
@@ -8,7 +8,8 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*str);
+		str++
 	}
 	_putchar('\n');
 }
