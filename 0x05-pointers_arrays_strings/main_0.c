5e1d8f8e85cb9390a7736bcfb0cbdda4410b353c
Geemonic <ogechi.okorochukwu@gmail.com>
2023-06-28 08:26:49 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/main_0.c
@@ -0,0 +1,14 @@
+
+t]
+-198, 298, 402, -1024,   98
+
+(28 chars long)
+	[stderr]: 
+	(0 chars long)
+	[Expected]
+	-198, 298, 402, -1024, 98
+
+	(26 chars long)
+	[stderr]: [Anything]
+	(0 chars long)
+
