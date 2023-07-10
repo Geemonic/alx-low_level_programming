e90197a4c3a1b0ec35f98fe79b7a43287da18ca4
Geemonic <ogechi.okorochukwu@gmail.com>
2023-07-10 10:11:01 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,7 @@
  */
 int count_word(char *s)
 {
-		int flag, c, w;
+		int flag; c, w;
 
 			flag = 0;
 				w = 0;
@@ -18,7 +18,7 @@ int count_word(char *s)
 						flag = 0;
 					else
 						if
-							[flag == 0]
+							(flag == 0);
 					flag = 0;
 					w++;
 				}
