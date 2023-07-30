c5bf9825c6f004dbb2664bb18f1a353085b2cd4c
Geemonic <ogechi.okorochukwu@gmail.com>
2023-07-30 02:59:18 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: the pointer to the list_t list to print
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t c = 0;
+
+	while
+		(h != NULL)
+		{
+			if (h->str == NULL)
+				printf("[0] (nil)\n");
+			else
+				printf("[%lu] %s\n", c, h->str);
+
+			c++;
+			h = h->next;
+		}
+
+	return (c);
+}
+
