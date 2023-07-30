a4344aa143bdf1bc5fcd130a9c322fea0394fcfa
Geemonic <ogechi.okorochukwu@gmail.com>
2023-07-30 04:21:59 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,25 +3,24 @@
 
 /**
  * print_list - prints all the elements of a linked list
- * @h: the pointer to the list_t list to print
+ * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
-	size_t c = 0;
+	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%lu] %s\n", c, h->str);
-
-		c++;
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
 
-	return (c);
+	return (s);
 }
 
